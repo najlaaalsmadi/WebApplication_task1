c85f1f8c5ef8548ad48d77723228d48e0982e38e
najlaaalsmadi <najlaak399@gmail.com>
2024-08-19 22:59:15 +0300

    Add project files.

---
+++ WebApplication_task1/Controllers/CategoriesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication_task1.Models;
+
+namespace WebApplication_task1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly MyDbContext _context;
+        public CategoriesController(MyDbContext context)
+        {
+            _context = context;
+
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var categories = _context.Categories.ToList();
+            return Ok(categories);
+        }
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var categoriesID = _context.Categories.Where(b => b.CategoryId == id);
+            return Ok(categoriesID);
+        }
+    }
+}
---
+++ WebApplication_task1/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication_task1.Models;
+
+namespace WebApplication_task1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly MyDbContext _context1;
+        public ProductsController(MyDbContext context)
+        {
+            _context1 = context;
+
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var Products = _context1.Products
+                .ToList();
+            return Ok(Products);
+        }
+        [HttpGet("Id")]
+        public IActionResult Get(int id)
+        {
+            var ProductsID = _context1.Products
+                .FirstOrDefault(b => b.ProductId == id);
+
+            return Ok(ProductsID);
+        }
+    }
+}
